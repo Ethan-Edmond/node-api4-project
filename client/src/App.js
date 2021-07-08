8eb0386db4cc2b5d11cc6e390062ebdfd6e880c3
Ethan Edmond <eedmond@uccs.edu>
2021-07-08 14:54:30 -0600

    Changed axios to work with th window location

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: window.href
+});
+
 function App() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
-//   axios.get('/api/users')
-//     .then(console.log)
-//     .catch(alert);
+    axios.get('/api/users')
+      .then(console.log)
+      .catch(alert);
   }, []);
 
   console.log(window.location);
