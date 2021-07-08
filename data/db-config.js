5ab0ca0ade29dfae064950306d85daa849effc2f
Ethan Edmond <eedmond@uccs.edu>
2021-07-08 13:28:06 -0600

    First attempt at bug fix

--- data/db-config.js
+++ data/db-config.js
@@ -2,5 +2,4 @@ const knex = require('knex');
 const configs = require('../knexfile.js');
 const environment = process.env.NODE_ENV || 'development';
 
-console.log( environment, process.env.NODE_ENV, 'development');
 module.exports = knex(configs[environment]);
